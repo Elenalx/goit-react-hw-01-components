2f691ca9da7d24fe2812c89426f161a4964a929a
Elena Blagodir <elenalx28@gmail.com>
2023-03-08 00:24:13 +0200

    number3

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Profile from 'components/Profile/Profile';
+import user from './Profile/user.json'
+
+const App = () => {
+    return <Profile/>
+}
+
+export default App
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,52 @@
+
+import css from './Profile.module.css'
+import PropTypes from 'prop-types'
+// import styled from 'styled-components'
+
+
+export const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes } ,
+}) => (
+  <div className={css.profile}>
+    <div className={css.description}>
+      <img
+        src={avatar}
+        alt={username}
+        className={css.avatar} />
+      <p className={css.userName}>{username}</p>
+      <p className={css.tagName}>{tag}</p>
+      <p className={css.locationName}>{location}</p>
+    </div>
+
+    <ul className={css.statsList}>
+      <li className={css.statsName}>
+        <span className={css.label}>Followers</span>
+        <span className={css.quantity}>{followers}</span>
+      </li>
+      <li className={css.statsName}>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>{views}</span>
+      </li>
+      <li className={css.statsName}>
+        <span className={css.label}>Likes</span>
+        <span className={css.quantity}>{likes}</span>
+      </li>
+    </ul>
+  </div>)
+
+
+  export default Profile;
+
+  Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.objectOf(PropTypes.number),
+}
+
+
